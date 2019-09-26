44ba41ccde13682e2b6306370b58c85e731437e7
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2019-09-26 10:39:57 +0200

    fix build system, introduce setup.py
    With the upgrade to Python 3, the distribution system broke. Introduce a
    setup.py to properly install the Python part.

---
+++ python/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+from distutils.core import setup
+
+setup(name='exxe',
+      version='0.8.2',
+      description='Execute commands indirectly',
+      author='Andreas Gruenbacher',
+      license='GPL v3.0',
+      author_email='andreas.gruenbacher@gmail.com',
+      url='https://github.com/LINBIT/exxe',
+      py_modules=['exxe'],
+      classifiers=[ ]
+      )
